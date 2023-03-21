29d28db26856bcd7daf87c2b3f634b770cf0d173
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-03-21 09:54:18 -0600

    initial commit

---
+++ src/app/components/timesheet/timesheet.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
+import { FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
+import { Employee } from '../../interfaces/employee'
+
+@Component({
+  selector: 'app-timesheet',
+  templateUrl: './timesheet.component.html',
+  styleUrls: ['./timesheet.component.scss']
+})
+
+export class TimesheetComponent implements OnInit {
+  departments: Department[]
+  department: Department
+  employeeNameFC = new FormControl('', this.nameValidator());
+  employees: Employee[] = [];
+  employeeId = 0;
+
+  constructor(
+      private route: ActivatedRoute,
+      private departmentsService: DepartmentsService,
+  ) { }
+
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+}
+
+addEmployee(): void {
+  if (this.employeeNameFC.value) {
+      this.employeeId++;
+
+      this.employees.push({
+          id: this.employeeId.toString(),
+          departmentId: this.department?.id,
+          name: this.employeeNameFC.value,
+          payRate: Math.floor(Math.random() * 50) + 50,
+      });
+
+      this.employeeNameFC.setValue('');
+  }
+}
+
+nameValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+      let error = null;
+      if (this.employees && this.employees.length) {
+          this.employees.forEach(employee => {
+              if (employee.name.toLowerCase() === control.value.toLowerCase()) {
+                  error = {duplicate: true};
+              }
+          });
+      }
+      return error;
+  };
+}
+
+}
---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
+
+  ]
+})
+export class MaterialModule { }
